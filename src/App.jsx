e70ed65eab8897ea915eb7dcd316287c40023aae
Joseph Uwagbale <joecrypt@yahoo.com>
2023-10-08 12:07:22 +0100

    finished vite set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import { createRoot } from 'react-dom'
+
 // Pet component
 const Pet = (props) => {
   return React.createElement("div", {}, [
@@ -31,5 +34,5 @@ const App = () => {
 };
 
 const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+const root = createRoot(container);
 root.render(React.createElement(App));
