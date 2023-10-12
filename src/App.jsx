cedad1a7ec1bcc2af4156b15689100b5cec8aaf3
Joseph Uwagbale <joecrypt@yahoo.com>
2023-10-12 22:46:40 +0100

    react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
-import { createRoot } from "react-dom";
-// import Pet from "./Pet";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
+import { Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// App coponent
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <div>
-      <h1>Adopt Me!</h1>
-      <SearchParams />
+      <BrowserRouter>
+        <QueryClientProvider client={queryClient}>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+          <Routes>
+            <Route path="/details/:id" element={<Details />} />
+            <Route path="/" element={<SearchParams />} />
+          </Routes>
+        </QueryClientProvider>
+      </BrowserRouter>
     </div>
-  )
+  );
 };
 
 const container = document.getElementById("root");
--- src/useBreedList.js
+++ src/useBreedList.js
@@ -1,34 +1,46 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 
-const localCache = {};
+// const localCache = {};
+
+// export default function useBreedList(animal) {
+//   const [breedList, setBreedList] = useState([]);
+//   const [status, setStatus] = useState("unloaded");
+
+//   useEffect(() => {
+//     if (!animal) {
+//       setBreedList([]);
+//     } else if (localCache[animal]) {
+//       // this is to not make request if localstorage object already has the object key
+//       setBreedList(localCache[animal]);
+//       console.log(localCache);
+//     } else {
+//       requestBreedList();
+//     }
+
+//     async function requestBreedList() {
+//       setBreedList([]);
+//       setStatus("loading");
+//       const res = await fetch(
+//         `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
+//       );
+//       const json = await res.json();
+//       localCache[animal] = json.breeds || []; // creating object property, animal as key and the array as value
+//       setBreedList(localCache[animal]);
+//       setStatus("loaded");
+//     }
+//   }, [animal]);
+
+//   return [breedList, status];
+// }
+
+//  USING REACT QUERY
+
+import { useQuery } from "@tanstack/react-query";
+import { fetchBreedList } from "./fetchBreedList";
 
 export default function useBreedList(animal) {
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const results = useQuery(["breeds", animal], fetchBreedList);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      // this is to not make request if localstorage object already has the object key
-      setBreedList(localCache[animal]);
-      console.log(localCache);
-    } else {
-      requestBreedList();
-    }
-
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache[animal] = json.breeds || []; // creating object property, animal as key and the array as value
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    }
-  }, [animal]);
-
-  return [breedList, status];
+  return [results?.data?.breeds ?? [], results.status];
+  // this makes the function return an empty array if results.data.breeds not available
 }
